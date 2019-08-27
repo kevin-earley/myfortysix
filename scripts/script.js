1e97f88a8a16e72a0f13c07314b67c2d55976e65
boogeydude <kearley89@gmail.com>
2019-08-27 15:24:26 -0400

    add js sort functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let highpeaks = [
+const highPeaksList = [
   {
     name: "Mt. Marcy",
     elevation: 5344,
@@ -14,6 +14,69 @@ let highpeaks = [
     elevation: 4960,
     completed: false
   },
+  {
+    name: "Mt. Skylight",
+    elevation: 4926,
+    completed: false
+  },
+  {
+    name: "Whiteface Mountain",
+    elevation: 4867,
+    completed: false
+  },
+  {
+    name: "Dix Mountain",
+    elevation: 4857,
+    completed: false
+  },
+  {
+    name: "Gray Peak",
+    elevation: 4840,
+    completed: false
+  }
 ];
 
-console.log(highpeaks);
+const handlers = {
+  sortByName: ( a, b ) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  },
+  sortByElevationHigh: ( a, b ) => {
+    return b.elevation - a.elevation;
+  },
+  sortByElevationLow: ( a, b ) => {
+    return a.elevation - b.elevation;
+  }
+}
+
+const view = {
+  displayHighPeaksByName: () => {
+    highPeaksList.sort( handlers.sortByName );
+    view.displayHighPeaks();
+  },
+  displayHighPeaksByElevationHigh: () => {
+    highPeaksList.sort( handlers.sortByElevationHigh );
+    view.displayHighPeaks();
+  },
+  displayHighPeaksByElevationLow: () => {
+    highPeaksList.sort( handlers.sortByElevationLow );
+    view.displayHighPeaks();
+  },
+  displayHighPeaks: () => {
+    const highPeaksUl = document.querySelector('ul#list-of-high-peaks');
+    highPeaksUl.innerHTML = '';
+
+    highPeaksList.forEach(mtn => {
+      let mtnLi = document.createElement("li");
+      mtnLi.textContent = "( ) " + mtn.name + " - " + mtn.elevation;
+      highPeaksUl.appendChild(mtnLi);
+    })
+  }
+}
+
+view.displayHighPeaks();
