42c687f1e6074281f092b3838d46ddf3e453811d
boogeydude <kearley89@gmail.com>
2019-08-27 10:54:22 -0400

    init commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let highpeaks = [
+  {
+    name: "Mt. Marcy",
+    elevation: 5344,
+    completed: false
+  },
+  {
+    name: "Algonquin Peak",
+    elevation: 5114,
+    completed: false
+  },
+  {
+    name: "Mt. Haystack",
+    elevation: 4960,
+    completed: false
+  },
+];
+
+console.log(highpeaks);
